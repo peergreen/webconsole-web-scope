eb1302951191749361294f75acba2a63964f6b90
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-09-17 18:06:56 +0200

    update copyright

--- src/main/java/com/peergreen/webconsole/scope/web/WebScope.java
+++ src/main/java/com/peergreen/webconsole/scope/web/WebScope.java
@@ -1,3 +1,14 @@
+/**
+ * Peergreen S.A.S. All rights reserved.
+ * Proprietary and confidential.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.peergreen.webconsole.scope.web;
 
 import com.peergreen.webconsole.Extension;
